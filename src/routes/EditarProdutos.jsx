2c00789c916f2c8f9339a6c729ae9910d7d7f275
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-04 11:47:39 -0300

    Produto recuperado no form.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -9,12 +9,32 @@ export default function EditarProdutos() {
     const {id} = useParams();
 
     const produtoRecuperadoDaListaById = ListaProdutos.filter(item => item.id == id );
-    
+
 
   return (
     <div>
         <h1>EDITAR - PRODUTOS</h1>
-        <p>PRODUTO SELECIONADO - {produtoRecuperadoDaListaById[0].nome}</p>
+
+        <div>
+          <form>
+            <fieldset>
+              <legend>PRODUTO SELECIONADO</legend>
+              <div>
+                <label htmlFor="idNome">Nome do Produto:</label>
+                <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperadoDaListaById[0].nome}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição do Produto:</label>
+                <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperadoDaListaById[0].desc}/>
+              </div>
+              <div>
+                <label htmlFor="idValor">Valor do Produto:</label>
+                <input type="text" name="valor" id="idValor" defaultValue={produtoRecuperadoDaListaById[0].valor}/>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+
     </div>
   )
 }
