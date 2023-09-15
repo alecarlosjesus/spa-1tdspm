7ce6068d70636c086c3bbf439e6849a94cb09c77
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-15 09:53:20 -0300

    Iniciando com CSS.MODULE, aplicando estilos na tabela com nesting

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -2,20 +2,17 @@ import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
+import style from "./Produtos.module.css";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  const estiloDasCelulas = {
-    border:"2px solid #ccc",
-    fontFamily:"Arial, Trebuchet, sans-serif"
-  }
 
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
 
-      <table style={{border:"2px solid #ccc", borderCollapse:"collapse", margin:"auto"}}>
+      <table className={style.tblEstilo}>
         <thead>
         <tr>
           <th>ID</th>
@@ -28,12 +25,12 @@ export default function Produtos() {
         <tbody>
         {
           ListaProdutos.map((item,indice)=>(
-            <tr key={indice}>
-                <td style={estiloDasCelulas}>{item.id}</td>
-                <td style={estiloDasCelulas}>{item.nome}</td>
-                <td style={estiloDasCelulas}>{item.desc}</td>
-                <td style={estiloDasCelulas}>{item.valor}</td>
-                <td style={estiloDasCelulas}> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${item.id}`}><Excluir/></Link> </td>
+            <tr key={indice} className={style.tblLine}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>{item.valor}</td>
+                <td> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${item.id}`}><Excluir/></Link> </td>
             </tr>
           ))
         }
